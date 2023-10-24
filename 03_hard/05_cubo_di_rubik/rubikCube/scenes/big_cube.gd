441ca8820c7f1b7a31d10f8edfb379d02fd83627
DESKTOP-LO4SJBG\justi <justin.janko.14@gmail.com>
2023-10-24 17:22:30 +0200

    [ADD] Added rubikCube to solve 05_cubo_di_rubik

---
+++ 03_hard/05_cubo_di_rubik/rubikCube/scenes/big_cube.gd
@@ -0,0 +1,361 @@
+extends Node3D
+
+class_name BigCube
+
+signal rotation_complete
+
+enum FACES { UP, LEFT, FRONT, RIGHT, DOWN, BACK }
+
+const PIVOT_POSITIONS = [
+	Vector3.UP,
+	Vector3.LEFT,
+	Vector3.BACK,
+	Vector3.RIGHT,
+	Vector3.DOWN,
+	Vector3.FORWARD
+]
+const INITIAL_FACE_MAP = [
+	FACES.UP,
+	FACES.LEFT,
+	FACES.FRONT,
+	FACES.RIGHT,
+	FACES.DOWN,
+	FACES.BACK
+]
+const CUBE_IDXS = [-1, 0, 1]
+const UPPERV = CUBE_IDXS[-1]
+const LOWERV = CUBE_IDXS[0]
+const X_AXIS_FACES = [FACES.UP, FACES.FRONT, FACES.DOWN, FACES.BACK]
+const Y_AXIS_FACES = [FACES.LEFT, FACES.FRONT, FACES.RIGHT, FACES.BACK]
+
+const EDGE_POSITIONS = [[0,1,-1],[-1,1,0],[1,1,0],[0,1,1],[-1,0,-1],[-1,0,1],[1,0,1],[1,0,-1],[-1,-1,0],[0,-1,1],[1,-1, 0],[0,-1,-1]]
+const CORNER_POSITIONS = [[-1,1,-1],[1,1,-1],[-1,1,1],[1,1,1],[-1,-1,-1],[-1,-1,1],[1,-1,1],[1,-1,-1]]
+const CENTER_POSITIONS = [[0,1,0],[-1,0,0],[0,0,1],[1,0,0],[0,-1,0],[0,0,-1]]
+
+const ROTATIONS = [0, 1, 2, -1]
+
+const PI2 = PI / 2
+
+@export var rotation_speed = 3.0
+
+var piece = preload("res://scenes/cube.tscn")
+var pivot = self
+
+# Keep a record of which of the 4 rotation positions each face is at
+var face_rotations = [0, 0, 0, 0, 0, 0]
+var face_rotating_idx := -1
+var face_rotation_angle = 0
+var face_rotation_direction := 1
+var rotation_dict = {}
+var faces_dict = {}
+var last_face_key = 0
+var edge_visibility = []
+var corner_visibility = []
+
+func _ready():
+	var cube_instance: Node3D = piece.instantiate()
+	fill_rotation_dict(cube_instance)
+	last_face_key = rotation_dict.keys()[0]
+	build_cube(cube_instance)
+	edge_visibility.resize(12)
+	corner_visibility.resize(8)
+	reset()
+
+
+func build_cube(cube_instance):
+	for x in CUBE_IDXS:
+		for y in CUBE_IDXS:
+			for z in CUBE_IDXS:
+				var cube = cube_instance.duplicate()
+				cube.position = Vector3(x, y, z)
+				cube.set_meta("initial_position", cube.position) # Useful when resetting
+				add_child(cube)
+				# Set face visibility
+				cube.get_child(FACES.UP).visible = y == UPPERV
+				cube.get_child(FACES.DOWN).visible = y == LOWERV
+				cube.get_child(FACES.RIGHT).visible = x == UPPERV
+				cube.get_child(FACES.LEFT).visible = x == LOWERV
+				cube.get_child(FACES.FRONT).visible = z == UPPERV
+				cube.get_child(FACES.BACK).visible = z == LOWERV
+
+
+func reset():
+	face_rotations = [0, 0, 0, 0, 0, 0]
+	for cube in get_children():
+		if cube is CSGBox3D:
+			cube.transform.basis = Basis()
+			cube.position = cube.get_meta("initial_position")
+			cube.set_grey(false)
+
+
+# Animate the cube group rotation
+func _process(delta):
+	if face_rotating_idx > -1:
+		var offset = face_rotations[face_rotating_idx]
+		face_rotation_angle = clamp(face_rotation_angle + delta * rotation_speed, 0, PI2)
+		rotate_group(face_rotating_idx, face_rotation_angle * face_rotation_direction + offset * PI2)
+		if face_rotation_angle >= PI2:
+			# Rotation complete
+			face_rotation_angle = 0
+			face_rotations[face_rotating_idx] = (offset + face_rotation_direction) % 4
+			face_rotating_idx = -1
+			reparent_to_origin()
+			emit_signal("rotation_complete")
+
+
+func rotate_faces_in_map(old_map, faces, offset):
+	var new_map = old_map.duplicate()
+	if offset > 0:
+		for idx in faces.size():
+			new_map[faces[idx]] = old_map[faces[(idx + offset) % 4]]
+	return new_map
+
+
+func rotate_face(idx, dir, bas):
+	if face_rotating_idx < 0:
+		var face_map = get_face_map_from_basis(bas)
+		idx = face_map[idx]
+		var group = get_group(idx)
+		reparent_to_pivot(group)
+		face_rotating_idx = idx
+		face_rotation_direction = get_correct_dir(idx, dir)
+
+
+func get_correct_dir(idx, dir):
+		# Correct the rotation direction according to the front face orientation
+		return dir if idx in [FACES.BACK, FACES.DOWN, FACES.LEFT] else -dir
+
+
+func rotate_face_immediate(idx, dir):
+	dir = get_correct_dir(idx, dir)
+	var group = get_group(idx)
+	reparent_to_pivot(group)
+	var offset = dir + face_rotations[idx]
+	rotate_group(idx, offset * PI2)
+	reparent_to_origin()
+	face_rotations[idx] = offset % 4
+
+
+func rotate_group(idx, rot_angle):
+	pivot.transform.basis = Basis() # reset rotation
+	match idx:
+		FACES.LEFT, FACES.RIGHT:
+			pivot.rotate_x(rot_angle)
+		FACES.UP, FACES.DOWN:
+			pivot.rotate_y(rot_angle)
+		FACES.FRONT, FACES.BACK:
+			pivot.rotate_z(rot_angle)
+
+
+func get_group(idx):
+	var group = []
+	for cube in get_children():
+		if PIVOT_POSITIONS[idx] == cube.position:
+			pivot = cube
+		# Using the DOT product here to find cubes on the same face as the pivot cube
+		if cube.position.dot(PIVOT_POSITIONS[idx]) > 0.5:
+			group.append(cube)
+	return group
+
+
+func reparent_to_pivot(group):
+	var node = pivot.get_node("Cubes")
+	for cube in group:
+		if cube != pivot:
+			cube.reparent(node)
+
+
+func reparent_to_origin():
+	if pivot != self:
+		# Reparent previously rotated child cubes to self
+		for cube in pivot.get_node("Cubes").get_children():
+			cube.reparent(self)
+
+###################################################
+# Functions to use with mapping colors to/from cube
+
+func get_edge_colors():
+	var edge_colors = []
+	var idx = 0
+	for node in get_edge_nodes():
+		edge_visibility[idx] = true if node.get_child(0).scale.x > 0.1 else false
+		edge_colors.append(get_colors_of_cube(node))
+		idx += 1
+	return edge_colors
+
+
+func get_corner_colors():
+	var corner_colors = []
+	var idx = 0
+	for node in get_corner_nodes():
+		corner_visibility[idx] = true if node.get_child(0).scale.x > 0.1 else false
+		corner_colors.append(get_colors_of_cube(node))
+		idx += 1
+	return corner_colors
+
+
+func get_edge_nodes():
+	var edge_nodes = []
+	for pos in EDGE_POSITIONS:
+		for node in get_children():
+			if (node.position - Vector3(pos[0], pos[1], pos[2])).length_squared() < 0.1:
+				edge_nodes.append(node)
+	return edge_nodes
+
+
+func get_corner_nodes():
+	var corner_nodes = []
+	for pos in CORNER_POSITIONS:
+		for node in get_children():
+			if (node.position - Vector3(pos[0], pos[1], pos[2])).length_squared() < 0.1:
+				corner_nodes.append(node)
+	return corner_nodes
+
+
+func get_center_nodes():
+	var center_nodes = []
+	for pos in CENTER_POSITIONS:
+		for node in get_children():
+			if (node.position - Vector3(pos[0], pos[1], pos[2])).length_squared() < 0.1:
+				center_nodes.append(node)
+	return center_nodes
+
+
+func get_colors_of_cube(node):
+	var colors = []
+	for tile_idx in 6:
+		var tile_pos = PIVOT_POSITIONS[tile_idx]
+		for color_idx in 6:
+			var pos = get_tile_position(node, color_idx)
+			if pos.dot(tile_pos) > 0.5:
+				colors.append(color_idx)
+	return colors
+
+
+func get_tile_position(node, color_idx):
+	return (node.get_child(color_idx).global_position - node.global_position).normalized()
+
+
+func set_edges(map_data):
+	var idx = 0
+	for node in get_edge_nodes():
+		# Find out where this node should be moved to
+		var edge_idx = map_data.edge_positions.find(idx)
+		var pos = EDGE_POSITIONS[edge_idx]
+		node.position = Vector3(pos[0], pos[1], pos[2])
+		set_cube_face_visibility(node, map_data.edge_visibility[edge_idx])
+		# Rotate the cube to align the faces correctly
+		rotate_cube(node, map_data.edge_face_map[edge_idx], map_data.edge_colors[edge_idx])
+		idx += 1
+
+
+func set_centers(map_data):
+	var idx = 0
+	for node in get_center_nodes():
+		set_cube_face_visibility(node, map_data.center_visibility[idx])
+		idx += 1
+
+
+func set_cube_face_visibility(cube, enable):
+	cube.set_grey(not enable)
+
+
+func rotate_cube(node, face_map, tile_colors):
+	var key = get_rotation_key(face_map, tile_colors)
+	var xyz = rotation_dict[key]
+	node.rotate_x(xyz[0] * PI2)
+	node.rotate_y(xyz[1] * PI2)
+	node.rotate_z(xyz[2] * PI2)
+
+
+func get_rotation_key(face_map, tile_colors):
+	for key in rotation_dict.keys():
+		var found = true
+		for idx in face_map.size():
+			if key[face_map[idx]] != tile_colors[idx]:
+				found = false
+				break # Try the next key
+		if found: return key
+
+
+func set_corners(map_data):
+	var idx = 0
+	for node in get_corner_nodes():
+		var corner_idx = map_data.corner_positions.find(idx)
+		var pos = CORNER_POSITIONS[corner_idx]
+		node.position = Vector3(pos[0], pos[1], pos[2])
+		set_cube_face_visibility(node, map_data.corner_visibility[corner_idx])
+		rotate_cube(node, map_data.corner_face_map[corner_idx], map_data.corner_colors[corner_idx])
+		idx += 1
+
+
+func apply_map(map_data):
+	reset()
+	set_edges(map_data)
+	set_corners(map_data)
+	set_centers(map_data)
+
+
+func get_face_position(node, face_idx):
+	var gp = node.get_child(face_idx).global_position
+	var np = node.global_position
+	var face_normal = (gp - np).normalized()
+	for idx in 6:
+		if face_normal.dot(PIVOT_POSITIONS[idx]) > 0.5:
+			return idx
+
+
+# Make lookup dictionary of face orientations vs rotations (in XYZ order)
+# There are 24 possible permutations of cube faces
+func fill_rotation_dict(cube):
+	add_child(cube)
+	for z in ROTATIONS:
+		for y in ROTATIONS:
+			for x in ROTATIONS:
+				var bcode = rotate_small_cube(cube, x, y, z)
+				var faces = get_faces_of_rotated_cube(cube)
+				if not rotation_dict.has(faces):
+					rotation_dict[faces] = [x,y,z]
+					faces_dict[bcode] = faces
+	remove_child(cube)
+
+
+func rotate_small_cube(cube, x, y, z):
+	cube.transform.basis = Basis() # reset rotation
+	cube.rotate_x(x * PI2)
+	cube.rotate_y(y * PI2)
+	cube.rotate_z(z * PI2)
+	return encode_basis(cube.transform.basis)
+
+
+func get_faces_of_rotated_cube(cube):
+	var faces = []
+	faces.resize(6)
+	for idx in 6:
+		faces[get_face_position(cube, idx)] = idx
+	return faces
+
+
+func encode_basis(b):
+	return get_sector(b.x.normalized())\
+		+ 13 * get_sector(b.y.normalized())\
+		+ 169 * get_sector(b.z.normalized())
+
+
+func get_sector(v):
+	const th = 0.6 # This affects sensitivity around the edges/corners
+	var n = 0
+	if v.x < th: n += 1
+	if v.x > th: n += 2
+	if v.y < -th: n += 3
+	if v.y > th: n += 4
+	if v.z < -th: n += 5
+	if v.z > th: n += 6
+	return n
+
+
+func get_face_map_from_basis(b):
+	var key = encode_basis(b.inverse())
+	if faces_dict.has(key):
+		last_face_key = key
+	return faces_dict[last_face_key]
---
+++ 03_hard/05_cubo_di_rubik/rubikCube/scenes/color_map.gd
@@ -0,0 +1,249 @@
+extends Control
+
+signal map_changed
+
+const COLORS = [Color.YELLOW, Color.RED, Color.GREEN, Color.ORANGE_RED, Color.WHITE, Color.BLUE, Color.DARK_GRAY]
+
+# See coordinates diagram for index relationships in media folder
+# The _FACE_MAP constants are indexing the faces
+# Copies of the FACE_MAP constants are indexing the changing colors on the faces of the pieces
+const CORNER_FACE_MAP = [[0,1,5],[0,5,3],[0,2,1],[0,3,2],[5,1,4],[1,2,4],[2,3,4],[3,5,4]]
+const EDGE_FACE_MAP = [[5,0],[1,0],[3,0],[2,0],[5,1],[1,2],[3,2],[3,5],[1,4],[2,4],[3,4],[5,4]]
+# FACE_ maps contain a list of edge/corner indices of parts on a face
+const FACE_CORNER_MAP = [[0,1,2,3],[0,2,4,5],[2,3,5,6],[3,1,6,7],[5,6,4,7],[4,7,0,1]]
+const FACE_EDGE_MAP = [[0,1,2,3],[1,4,5,8],[3,5,6,9],[2,6,7,10],[9,8,10,11],[11,4,7,0]]
+# This indexes the positions of faces under the Grid node
+const GRID_FACE_MAP = [1,3,4,5,7,10]
+const BLACK_IDX = 6
+
+var source_tile = null
+var edges = [] # 12 sets of 2 colors
+var corners = [] # 8 sets of 3 colors
+var center_visibility = []
+var edge_visibility = []
+var corner_visibility = []
+var solving = false
+
+func _ready():
+	# Set up the tiles
+	var tile = ColorRect.new()
+	tile.custom_minimum_size = Vector2(32, 32)
+	var idx = 0
+	for node in $Grid.get_children():
+		if node is GridContainer:
+			for n in 9:
+				var new_tile = tile.duplicate()
+				new_tile.set_meta("id", { "face": idx, "tile": n })
+				# immovable middle cell when n == 4
+				new_tile.gui_input.connect(handle_click.bind(new_tile, n != 4))
+				node.add_child(new_tile)
+			idx += 1
+	reset()
+
+
+func reset():
+	# Need deep copy so that inner arrays may be modified
+	edges = EDGE_FACE_MAP.duplicate(true)
+	corners = CORNER_FACE_MAP.duplicate(true)
+	center_visibility.resize(6)
+	center_visibility.fill(true)
+	edge_visibility.resize(12)
+	edge_visibility.fill(true)
+	corner_visibility.resize(8)
+	corner_visibility.fill(true)
+	var color_idx = 0
+	for node in $Grid.get_children():
+		if node is GridContainer:
+			for tile in node.get_children():
+				tile.color = COLORS[color_idx]
+			color_idx += 1
+
+
+func solved():
+	return edges.hash() == EDGE_FACE_MAP.hash() && corners.hash() == CORNER_FACE_MAP.hash()
+
+
+func handle_click(ev: InputEvent, clicked_tile, moveable):
+	if ev is InputEventMouseButton and ev.pressed and not solving:
+		# If shift is pressed, toggle the visibility of the tile
+		if Input.is_key_pressed(KEY_SHIFT) or Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			var id = clicked_tile.get_meta("id")
+			if is_edge(id.tile):
+				var edge_idx = get_edge(id.face, id.tile)
+				edge_visibility[edge_idx] = !edge_visibility[edge_idx]
+				set_edge_color(edge_idx, 0)
+				set_edge_color(edge_idx, 1)
+			elif is_corner(id.tile):
+				var corner_idx = get_corner(id.face, id.tile)
+				corner_visibility[corner_idx] = !corner_visibility[corner_idx]
+				for n in 3:
+					set_corner_color(corner_idx, n)
+			else:
+				center_visibility[id.face] = !center_visibility[id.face]
+				var color_idx = id.face if center_visibility[id.face] else BLACK_IDX
+				set_tile_color(id.face, id.tile, color_idx)
+			emit_signal("map_changed")
+		elif moveable:
+			if source_tile:
+				source_tile.color = COLORS[source_tile.get_meta("id").face]
+				try_move(source_tile, clicked_tile)
+				source_tile = null
+			else:
+				source_tile = clicked_tile
+				source_tile.color = clicked_tile.color.darkened(0.2)
+
+
+func try_move(from_tile, to_tile):
+	var from = from_tile.get_meta("id")
+	var to = to_tile.get_meta("id")
+	if from == to: return
+	# if moving in same group of cells then rotating the colors
+	# corners move to corners
+	# edges move to edges
+	# middle cells can't be moved
+	if is_edge(from.tile) and is_edge(to.tile):
+		var edge1 = get_edge(from.face, from.tile)
+		var edge2 = get_edge(to.face, to.tile)
+		if edge1 == edge2:
+			# swap colors
+			edges[edge1].reverse()
+			for face_idx in 2:
+				set_edge_color(edge1, face_idx)
+		else:
+			# Want from.face tile to move to to.face tile for a natural feel
+			if EDGE_FACE_MAP[edge1].find(from.face) != EDGE_FACE_MAP[edge2].find(to.face):
+				# swap colors
+				edges[edge1].reverse()
+			# swap edges
+			var edge1_colors = edges[edge1]
+			edges[edge1] = edges[edge2]
+			edges[edge2] = edge1_colors
+			for face_idx in 2:
+				set_edge_color(edge1, face_idx)
+				set_edge_color(edge2, face_idx)
+	if is_corner(from.tile) and is_corner(to.tile):
+		var corner1 = get_corner(from.face, from.tile)
+		var corner2 = get_corner(to.face, to.tile)
+		if corner1 == corner2:
+			# rotate colors
+			corners[corner1] = get_rotated_faces(corner1, from.face, corner1, to.face)
+			for face_idx in 3:
+				set_corner_color(corner1, face_idx)
+		else:
+			# swap corners
+			var rotated_corner1 = get_rotated_faces(corner1, from.face, corner2, to.face)
+			corners[corner1] = corners[corner2]
+			corners[corner2] = rotated_corner1
+			for face_idx in 3:
+				set_corner_color(corner1, face_idx)
+				set_corner_color(corner2, face_idx)
+	emit_signal("map_changed")
+
+
+func get_rotated_faces(corner1, from_idx, corner2, to_idx):
+	var rot_step = wrapi(CORNER_FACE_MAP[corner1].find(from_idx) - CORNER_FACE_MAP[corner2].find(to_idx), -1, 2)
+	return [corners[corner1][rot_step], corners[corner1][rot_step + 1], corners[corner1][wrapi(rot_step + 2, 0, 3)]]
+
+
+func is_edge(n):
+	return n % 2 == 1 # Odd number
+
+
+func is_corner(n):
+	return n in [0,2,6,8]
+
+
+func get_edge(face, n):
+	return FACE_EDGE_MAP[face][n / 2]
+
+
+func get_corner(face, n):
+	return FACE_CORNER_MAP[face][(n + 1) / 3]
+
+
+func set_edge_color(edge_idx, face_idx):
+	# Get the face of the grid
+	var face = EDGE_FACE_MAP[edge_idx][face_idx]
+	# Get the tile idx within the face
+	var tile = FACE_EDGE_MAP[face].find(edge_idx) * 2 + 1
+	var color = edges[edge_idx][face_idx] if edge_visibility[edge_idx] else BLACK_IDX
+	set_tile_color(face, tile, color)
+
+
+func set_corner_color(corner_idx, face_idx):
+	var face = CORNER_FACE_MAP[corner_idx][face_idx]
+	var tile = FACE_CORNER_MAP[face].find(corner_idx) * 3 / 2 * 2
+	var color = corners[corner_idx][face_idx] if corner_visibility[corner_idx] else BLACK_IDX
+	set_tile_color(face, tile, color)
+
+
+func set_tile_color(face_idx, tile_idx, color_idx):
+	$Grid.get_child(GRID_FACE_MAP[face_idx]).get_child(tile_idx).color = COLORS[color_idx]
+
+
+# Input array of arrays of 6 cube face colors
+func set_edge_colors(edge_cube_colors, _edge_visibility):
+	edge_visibility = _edge_visibility
+	var idx = 0
+	for ec in edge_cube_colors:
+		# We want to extract the 2 face colors of the edge from the 6 faces of the cube
+		edges[idx] = []
+		for n in 2:
+			edges[idx].append(ec[EDGE_FACE_MAP[idx][n]])
+			set_edge_color(idx, n)
+		idx += 1
+
+
+# Input array of arrays of 6 cube face colors
+func set_corner_colors(corner_cube_colors, _corner_visibility):
+	corner_visibility = _corner_visibility
+	var idx = 0
+	for cc in corner_cube_colors:
+		corners[idx] = []
+		# Extract 3 face colors for the corner
+		for n in 3:
+			corners[idx].append(cc[CORNER_FACE_MAP[idx][n]])
+			set_corner_color(idx, n)
+		idx += 1
+
+
+# Search for the matching sets of colors to identify the edges
+# The index of the initial edge position is stored in the new position
+func get_edge_positions():
+	var positions = []
+	for n in 12:
+		var cols = edges[n]
+		positions.append(get_edge_position(cols))
+	return positions
+
+
+func get_edge_position(cols):
+	if !EDGE_FACE_MAP.has(cols):
+		cols = [cols[1], cols[0]]
+	return EDGE_FACE_MAP.find(cols)
+
+
+func get_corner_positions():
+	var positions = []
+	for n in 8:
+		var cols = corners[n]
+		if !CORNER_FACE_MAP.has(cols):
+			cols = [cols[2], cols[0], cols[1]]
+			if !CORNER_FACE_MAP.has(cols):
+				cols = [cols[2], cols[0], cols[1]]
+		positions.append(CORNER_FACE_MAP.find(cols))
+	return positions
+
+
+func get_data():
+	return {
+		"edge_positions": get_edge_positions(),
+		"edge_colors": edges,
+		"edge_face_map": EDGE_FACE_MAP,
+		"corner_positions": get_corner_positions(),
+		"corner_colors": corners,
+		"corner_face_map": CORNER_FACE_MAP,
+		"center_visibility": center_visibility,
+		"edge_visibility": edge_visibility,
+		"corner_visibility": corner_visibility
+	}
