885632e59f1b2546f40fcb4e32267679b5870393
turtulin <mm.martamusso@gmail.com>
2023-10-24 13:06:45 +0200

    Array sorting

---
+++ 01_easy/01_array_sorting/arraySorting.java
@@ -0,0 +1,22 @@
+public static int[] selectionSort(int[] array) {
+  // Creo una copia dell'array in input per non toccare l'originale
+  int[] sortedArray = array.clone();
+  int n = sortedArray.length;
+  for (int i = 0; i < n - 1; i++) {
+    // Inizializzo l'indice del minimo elemento come i
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++) {
+      // Se trovo un elemento minore del minimo attuale, aggiorno l'indice del minimo
+      if (sortedArray[j] < sortedArray[minIndex]) {
+        minIndex = j;
+      }
+    }
+    // Se l'indice del minimo è diverso da i, scambio i due elementi
+    if (minIndex != i) {
+      int temp = sortedArray[i];
+      sortedArray[i] = sortedArray[minIndex];
+      sortedArray[minIndex] = temp;
+    }
+  }
+  return sortedArray;
+}
