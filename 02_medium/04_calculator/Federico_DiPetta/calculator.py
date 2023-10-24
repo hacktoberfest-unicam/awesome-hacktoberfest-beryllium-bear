a2fd44d0835533c350a3a46785c14260c161b140
zombifede <98073432+zombifede@users.noreply.github.com>
2023-10-24 17:50:11 +0200

    [add]add calculator gui

---
+++ 02_medium/04_calculator/Federico_DiPetta/calculator.py
@@ -0,0 +1,27 @@
+def sum(a,b) : 
+    return a+b 
+def mul(a,b) : 
+    return a*b
+def dif(a,b) :
+    return a-b
+def div(a,b) : 
+    return a/b
+
+def number () :
+    val = val 
+    
+def operator (x) : 
+    if(x == "+" ) : 
+            return sum
+    elif(x == "-") :
+            return dif 
+    elif(x == "*" ) :
+         return mul
+    elif(x=="/") :
+         return div
+    RuntimeError
+
+def apply (func, a , b) :
+     return func (a,b)
+
+        
