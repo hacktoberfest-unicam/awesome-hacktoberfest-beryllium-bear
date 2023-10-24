8f0167b58b3106b75fc3cc30017a3954b43c23ab
Nukleo <138380587+nukleoyt@users.noreply.github.com>
2023-10-24 14:59:54 +0200

    Delete 01_easy/01_array_sorting/denisirmiciarray directory

--- 01_easy/01_array_sorting/denisirmiciarray/arraydenis.py
+++
@@ -1,8 +0,0 @@
-a1 = [100, 1000, -1111, -1.3, -3, 100032]
-
-for i in range(0, len(a1)):
-    for k in range(i+1, len(a1)):
-        if(a1[i]>=a1[k]):
-            a1[i], a1[k]=a1[k], a1[i]
-
-print("Array sortato",a1)
