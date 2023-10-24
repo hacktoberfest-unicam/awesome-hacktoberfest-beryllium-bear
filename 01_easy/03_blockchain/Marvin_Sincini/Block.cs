e6dc663085bf2b3779ff63157639006ed9c95434
Marvin Sincini <118466836+Sassas98@users.noreply.github.com>
2023-10-24 15:26:03 +0200

    [add] Aggiunta la blockchain.

---
+++ 01_easy/03_blockchain/Marvin_Sincini/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain {
+    public class Block {
+
+        public string stringaAttuale { get; init; }
+        public int hashPrecedente { get; init; }
+
+        public Block(string stringaAttuale, int hashPrecedente ) {
+            this.stringaAttuale = stringaAttuale;
+            this.hashPrecedente = hashPrecedente;
+        }
+
+        public override int GetHashCode() {
+            return stringaAttuale.GetHashCode();
+        }
+
+        public string GetValue(int hash) {
+            return hash == hashPrecedente ? stringaAttuale: "";
+        }
+
+        public string GetValue(string stringa) {
+            return GetValue(stringa.GetHashCode());
+        }
+
+    }
+}
---
+++ 01_easy/03_blockchain/Marvin_Sincini/Chain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain {
+    public class Chain {
+
+        private const string START = "start";
+        private List<Block> catena;
+
+        public Chain() {
+            catena = new List<Block>();
+        }
+
+        public int GetLastHash() {
+            if(catena.Count == 0) 
+                return START.GetHashCode();
+            return catena[catena.Count-1].GetHashCode();
+        }
+
+        public bool AddNewBlock(string block) {
+            return AddNewBlock(new Block(block, GetLastHash()));
+        }
+
+        public bool AddNewBlock(Block block) {
+            if (block.hashPrecedente == GetLastHash()) {
+                catena.Add(block);
+                return true;
+            }
+            return false;
+        }
+
+        public int GetSize() {
+            return this.catena.Count;
+        }
+
+        public string? GetAt(int i) {
+            if (i < 0 || i >= this.catena.Count)
+                return null;
+            return this.catena[i].stringaAttuale;
+        }
+
+    }
+}
