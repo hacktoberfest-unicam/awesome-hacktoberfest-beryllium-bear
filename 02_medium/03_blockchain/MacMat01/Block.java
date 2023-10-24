8a4898f71e61f4552c76f2689fc24cf71eb68945
MacMat <matteo.machella01@gmail.com>
2023-10-24 16:50:43 +0200

    Committed Improvement of blockchain task

--- 02_medium/03_blockchain/MacMat01/Block.java
+++ 02_medium/03_blockchain/MacMat01/Block.java
@@ -10,12 +10,14 @@ public class Block {
     private final String data;
     private final String previousHash;
     private final String hash;
+    private final int nonce;
 
-    public Block(int index, String data, String previousHash) {
+    public Block(int index, String data, String previousHash, int difficulty) {
         this.index = index;
         this.timestamp = System.currentTimeMillis();
         this.data = data;
         this.previousHash = previousHash;
+        this.nonce = mineBlock(difficulty);
         this.hash = calculateHash();
     }
 
@@ -39,8 +41,12 @@ public class Block {
         return hash;
     }
 
+    public int getNonce() {
+        return nonce;
+    }
+
     public String calculateHash() {
-        String dataToHash = index + timestamp + data + previousHash;
+        String dataToHash = index + timestamp + data + previousHash + nonce;
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(dataToHash.getBytes());
@@ -53,22 +59,36 @@ public class Block {
             throw new RuntimeException(e);
         }
     }
+
+    private int mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        int nonce = 0;
+        while (true) {
+            String attempt = calculateHash();
+            if (attempt.startsWith(target)) {
+                return nonce;
+            }
+            nonce++;
+        }
+    }
 }
 
 class Blockchain {
     private final List<Block> chain;
+    private final int difficulty; // Adjust the difficulty level.
 
-    public Blockchain() {
+    public Blockchain(int difficulty) {
         chain = new ArrayList<>();
+        this.difficulty = difficulty;
         // Create the genesis block (the first block in the chain)
-        chain.add(new Block(0, "Genesis Block", "0"));
+        chain.add(new Block(0, "Genesis Block", "0", difficulty));
     }
 
     public Block createBlock(String data) {
         Block previousBlock = chain.get(chain.size() - 1);
         int newIndex = previousBlock.getIndex() + 1;
         String previousHash = previousBlock.getHash();
-        Block newBlock = new Block(newIndex, data, previousHash);
+        Block newBlock = new Block(newIndex, data, previousHash, difficulty);
         chain.add(newBlock);
         return newBlock;
     }
@@ -95,8 +115,10 @@ class Blockchain {
             System.out.println("Timestamp: " + block.getTimestamp());
             System.out.println("Data: " + block.getData());
             System.out.println("Previous Hash: " + block.getPreviousHash());
+            System.out.println("Nonce: " + block.getNonce());
             System.out.println("Hash: " + block.getHash());
             System.out.println();
         }
     }
 }
+
