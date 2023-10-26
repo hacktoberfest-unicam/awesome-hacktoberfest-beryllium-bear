c423ac86b0491df2963c629e7cff8ca33750b378
Lorenzo Lapucci <halexercraft@gmail.com>
2023-10-26 17:32:59 +0200

    Update BlockValidator.java

--- 02_medium/03_blockchain/MacMat01/BlockValidator.java
+++ 02_medium/03_blockchain/MacMat01/BlockValidator.java
@@ -7,20 +7,6 @@ public class BlockValidator {
         }
 
         // Rule 2: The new block's previousHash should match the hash of the previous block.
-        if (!newBlock.getPreviousHash().equals(previousBlock.getHash())) {
-            return false;
-        }
-
-        // Rule 3: The new block's hash should be valid (e.g., meet a certain difficulty target).
-        return isValidBlockHash(newBlock.getHash());
-    }
-
-    // Add more validation rules as needed.
-
-    private static boolean isValidBlockHash(String hash) {
-        // You can define a difficulty target or other criteria for a valid hash.
-        // For example, the hash should start with a certain number of leading zeros.
-        // Implement your specific criteria here.
-        return true; // Replace with your validation logic.
+        return newBlock.getPreviousHash().equals(previousBlock.getHash());
     }
 }
